ca4c8dbe76ae7dd0987568426912fcb73ebc3d28
Altair <altair.ajj@gmail.com>
2021-07-26 21:47:45 -0300

    feature: rotas com switch

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const http = require('http')
 
 http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
+        'Content-Type': 'text/html; charset=utf-8'
     })
-    res.end('Servidor funcionando redondinho, ó!')
+    switch (req.url) {
+        case '/':
+          return res.end('<h1>VOCÊ ESTÁ NA HOME</h1>')
+        case '/sobre':
+          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+        case '/produto':
+          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
+        default:
+          return res.end(`<h1>NÃO EXISTE ESSA PÁGINA${req.url.toUpperCase().replace(/\/|-/g,' ')}</h1>`)
+      }
+    
 }).listen(3000, 'localhost', () => console.log('O servidor está rodando na porta 3000'))
