3a31dbabc6055f331cc55f1908895fe134b3f18a
Altair <altair.ajj@gmail.com>
2021-07-28 20:05:18 -0300

    feature: servidor e rota inicial com express

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const http = require('http')
+const express = require('express'),
+  app = express()
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-    })
-    switch (req.url) {
-        case '/':
-          return res.end('<h1>VOCÊ ESTÁ NA HOME</h1>')
-        case '/sobre':
-          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
-        case '/produto':
-          return res.end('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
-        default:
-          return res.end(`<h1>NÃO EXISTE ESSA PÁGINA${req.url.toUpperCase().replace(/\/|-/g,' ')}</h1>`)
-      }
-    
-}).listen(3000, 'localhost', () => console.log('O servidor está rodando na porta 3000'))
+  app.get('/', (req, res) => res.send('Olá mundo'))
+
+  app.listen(3000, ()=> console.log('Servidor iniciado'))
