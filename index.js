a1909fe2886ffae4adc17ef37a67a78d5e1ef179
Altair <altair.ajj@gmail.com>
2021-07-28 20:18:24 -0300

    feature: instalando nodemon como dev dependence

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'),
   app = express()
 
-  app.get('/', (req, res) => res.send('Olá mundo'))
+  app.get('/', (req, res) => res.send('<h3>Vocês está na página inicial</h3>'))
 
   app.listen(3000, ()=> console.log('Servidor iniciado'))
